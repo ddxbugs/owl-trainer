9621ca6e9247bdf7c9ef1ba6cf520fb968c33798
ddxbugs <ddxbugs@proton.me>
2023-12-26 21:28:26 -0800

    app/ initial commit

---
+++ app/app.R
@@ -0,0 +1,70 @@
+#========================================================
+# Project: Overwatch R Shiny App
+# Script Name: app.R
+# Author: ddxbugs
+# Date: 2023-12-26
+# Last Modified: 2023-12-26
+# Version: 1.0.0-alpha.1+001
+#========================================================
+# Description:
+# This script performs [brief description of what the script does, 
+# e.g., data cleaning, data visualization, statistical analysis, etc.]
+#========================================================
+# Usage:
+# [Instructions on how to use this script, if necessary]
+#========================================================
+# Notes:
+# [Any additional notes, warnings, dependencies, or other relevant information]
+#========================================================
+
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
