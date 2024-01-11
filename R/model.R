1291444218ff30aed577356a9e715a5916645dd9
ddxbugs <ddxbugs@proton.me>
2024-01-10 20:58:57 -0800

    R/model.R add header session info working directory import classification regression training libraries

---
+++ R/model.R
@@ -0,0 +1,30 @@
+#========================================================
+# Project: Overwatch Exploratory Data Analysis with R
+# Script Name: model.R
+# Author: ddxbugs
+# Date: 2024-01-10
+# Last Modified: 2024-01-10
+# Version: 1.0.0-alpha.1+001
+#========================================================
+# Description:
+# This script performs [brief description of what the script does, 
+# e.g., data cleaning, data visualization, statistical analysis, etc.]
+#========================================================
+# Usage:
+# [Instructions on how to use this script, if necessary]
+#========================================================
+# Notes:
+# [Any additional notes, warnings, dependencies, or other relevant information]
+#========================================================
+
+# Print session info
+sessionInfo()
+# Set working directory
+setwd("data/")
+
+#========================================================
+# Import Data
+#========================================================
+
+library(caret)
+library(class)
