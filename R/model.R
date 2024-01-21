308393d3fa6eaefa686dc2ababfe6af977d921bb
ddxbugs <ddxbugs@proton.me>
2024-01-20 16:05:11 -0800

    R/model.R refactor variable

--- R/model.R
+++ R/model.R
@@ -81,10 +81,10 @@ df <- df %>%
 df <- df %>%
   separate(game_length, sep=":", into=c("min","sec"),convert=TRUE, extra="drop")
 
-# Filter and summarize mean elim, assist, death by control_no, team, result
+# Filter and summarize mean elim, assist, death by group_id, team, result
 df_mean_ead <- df %>%
   filter(comp == "no") %>%
-  group_by(control_no, team, result) %>%
+  group_by(group_id, team, result) %>%
   summarize(meanElim=mean(elimination), meanAsst=mean(assist), meanDeath=mean(death), .groups="drop")
 
 #========================================================
